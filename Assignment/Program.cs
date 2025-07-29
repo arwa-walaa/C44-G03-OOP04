1215088eee52b2049a9afc20416cb7919d1932c4
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-07-29 21:57:54 +0300

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,11 +5,22 @@
         static void Main(string[] args)
         {
             #region Q1: Calculator with overloaded Add methods
-            Calculator calculator = new Calculator();
-            Console.WriteLine("Add two integers: " + calculator.Add(5, 10)); // Output: 15
-            Console.WriteLine("Add three integers: " + calculator.Add(5, 10, 15)); // Output: 30
-            Console.WriteLine("Add two doubles: " + calculator.Add(5.5, 10.5)); // Output: 16.0
+            //Calculator calculator = new Calculator();
+            //Console.WriteLine("Add two integers: " + calculator.Add(5, 10)); // Output: 15
+            //Console.WriteLine("Add three integers: " + calculator.Add(5, 10, 15)); // Output: 30
+            //Console.WriteLine("Add two doubles: " + calculator.Add(5.5, 10.5)); // Output: 16.0
             #endregion
+
+            #region Q2: Rectangle with multiple constructors
+            Rectangle rect1 = new Rectangle(); 
+            Rectangle rect2 = new Rectangle(5, 10);
+            Rectangle rect3 = new Rectangle(7); 
+            Console.WriteLine($"Rect1: Width = {rect1.Width}, Height = {rect1.Height}");
+            Console.WriteLine($"Rect2: Width = {rect2.Width}, Height = {rect2.Height}"); 
+            Console.WriteLine($"Rect3: Width = {rect3.Width}, Height = {rect3.Height}");
+            #endregion
+
+
         }
     }
 }
---
+++ Assignment/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        // Parameterless constructor
+        public Rectangle()
+        {
+            Width = 0;
+            Height = 0;
+        }
+
+        // Constructor with width and height
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        // Constructor with single value for both dimensions
+        public Rectangle(int size)
+        {
+            Width = size;
+            Height = size;
+        }
+    }
+}
