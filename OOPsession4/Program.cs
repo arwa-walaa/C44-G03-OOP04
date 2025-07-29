1c423f811f79de37af9fa13dbd297c8afe9e2d6d
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-07-29 19:13:17 +0300

    Add project files.

---
+++ OOPsession4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPsession4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
