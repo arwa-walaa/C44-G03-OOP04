16e88f1f480e2e43d467e333017563b43c306106
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-07-29 19:30:21 +0300

    PolyMorphism

--- OOPsession4/Program.cs
+++ OOPsession4/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        #region Methods overloading
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static int sum(int a, int b,int d)
+        {
+            return a + b+d;
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Polymorphism
+            Console.WriteLine(sum(1, 2));
+            Console.WriteLine(sum(2, 3,4));
+            Console.Write(sum(3.5, 4.5));
+            #endregion
         }
     }
 }
